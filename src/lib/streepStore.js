a3cb43d8e2f5602826200b83aad6cd8d9e4ca206
Ronald van Meurs <ronald@kantoor.dewinter.nl>
2022-04-07 16:40:25 +0200

    werk pc

--- src/lib/streepStore.js
+++ src/lib/streepStore.js
@@ -7,10 +7,14 @@ export const loading = writable(false);
 
 export const loadTodos = async () => {
     loading.set(true)
-    const {data, error} = await supabase
-    .from('strepen')
-    .select(`id,aantal, betaald, gebruiker (naam_kort) `)
+    // const {data, error} = await supabase
+    // .from('strepen')
+    // .select(`id,aantal, betaald, gebruiker (naam_kort) `)
    
+    const { data, error } = await supabase
+    .rpc('aantal').select('*')
+    console.log(data)
+
   if (error) throw new Error(error.message)
   
     strepen.set(data);
